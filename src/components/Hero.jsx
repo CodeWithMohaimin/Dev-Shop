dbd2a6c006124ebca7a288c0d1e7d9edb550d012
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-06 22:20:23 +0600

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import heroImage from '../image/hero-image.png'
 
+import Lottie from 'lottie-react'
+import Learning from '../learning.json'
 
 const Hero = () => {
     return (
-        <div className='bg-hero-background bg-no-repeat bg-center h-screen'>
-            <section className=' mt-4 flex justify-center lg:justify-between flex-col lg:flex-row gap-8 items-center max-w-7xl mx-auto p-1 lg:p-2 '>
+        <section className='bg-hero-background bg-no-repeat bg-center h-screen flex justify-center items-center'>
+            <div className=' mt-4 flex justify-center lg:justify-between flex-col lg:flex-row gap-8 items-center max-w-7xl mx-auto p-1 lg:p-2 '>
                 <div className='text-center lg:text-left max-w-xl w-full'>
-                    <h1 className='text-3xl lg:text-5xl font-semibold mb-6 text-gray-600'><span className='text-pink-500 text-6xl'>Dev. Shop</span> <br /> for developer's to solve their Problems</h1>
+
+                    <h1 className='text-3xl lg:text-4xl font-semibold mb-6 text-gray-600'><span className=' text-6xl text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-teal-500'>Dev. Shop</span> <br /> for developer's to solve their Problems</h1>
+
                     <p className='text-gray-500 font-medium text-lg'>Here you find many kinds of Navbar, Cards and Images for Developer uses. Here you get all source code for develop Navbar, Cards and much more. Don't just copy and past those codes, But also try to Understand those codes.</p>
-                    <button className='px-8 py-2  text-gray-600 mt-8 font-semibold text-1xl rounded-full bg-pink-200 hover:bg-pink-400 transition-colors hover:text-pink-50 tracking-wide'>Join Slack</button>
+                    
+                    <button className='transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 duration-300 px-8 py-2 mt-8 font-bold text-1xl rounded-full text-pink-50 tracking-wide bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:transition-all hover:to-yellow-500'>Join Slack</button>
                 </div>
-                <div className='max-w-2xl'>
-                    <img className='w-full p-2' src={heroImage} alt="Hero section" />
+                <div className='max-w-4xl w-full'>
+                <Lottie className='' animationData={Learning} />
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
         
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+// import logo from '../image/ode.svg'
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <section>
+      {/* <div className='max-w-7xl mx-auto px-2 py-6 bg-gray-300'>
+        
+      </div> */}
+
+      <div>
+     
+      </div>
+    </section>
   )
 }
 
