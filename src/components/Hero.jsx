6331ff32bf5ff83f0ea3227278d5744542796940
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-06 23:02:14 +0600

    added lottie files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ import Learning from '../learning.json'
 const Hero = () => {
     return (
         <section className='bg-hero-background bg-no-repeat bg-center h-screen flex justify-center items-center'>
-            <div className=' mt-4 flex justify-center lg:justify-between flex-col lg:flex-row gap-8 items-center max-w-7xl mx-auto p-1 lg:p-2 '>
-                <div className='text-center lg:text-left max-w-xl w-full'>
+            <div className=' mt-4 flex justify-center lg:justify-between flex-col-reverse lg:flex-row gap-8 items-center max-w-7xl mx-auto p-1 lg:p-2 '>
+                <div className='text-center lg:text-left max-w-lg w-full'>
 
                     <h1 className='text-3xl lg:text-4xl font-semibold mb-6 text-gray-600'><span className=' text-6xl text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-teal-500'>Dev. Shop</span> <br /> for developer's to solve their Problems</h1>
 
@@ -15,7 +15,7 @@ const Hero = () => {
                     
                     <button className='transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 duration-300 px-8 py-2 mt-8 font-bold text-1xl rounded-full text-pink-50 tracking-wide bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:transition-all hover:to-yellow-500'>Join Slack</button>
                 </div>
-                <div className='max-w-4xl w-full'>
+                <div className='w-full'>
                 <Lottie className='' animationData={Learning} />
                 </div>
             </div>
