0f41e335da462a1000b5680f7c4a17ef66c5cf68
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-07 09:39:46 +0600

    done navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import Lottie from "lottie-react";
 import logo from "../lottie-json/logo.json";
 
-import bun from "../image/menu-icon.svg";
-import cross from "../image/close-icon.svg";
 import { Link } from "react-router-dom";
+
+// import { VscMenu } from "react-icons/vsc";
+// import { VscClose } from "react-icons/vsc";
+
+import menu from "../image/menu-icon.svg";
+import close from "../image/close-icon.svg";
 
 const Links = [
   { name: "Home", link: "/" },
@@ -13,9 +18,11 @@ const Links = [
 ];
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <section className="shadow-md">
-      <div className="max-w-7xl mx-auto px-4 py-6 bg-transparent flex items-center justify-between">
+    <section className="shadow-md static top-0 left-0">
+      <div className="max-w-7xl mx-auto sm:px-4 bg-transparent flex items-center justify-between ">
         <div className="flex items-center">
           <Lottie className="w-20 h-20" animationData={logo} />
           <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-pink-500 text-2xl lg:text-5xl ">
@@ -23,10 +30,16 @@ const Navbar = () => {
           </h1>
         </div>
 
-        <div>
-          <ul className="sm:flex gap-6 absolute sm:static bg-gray-400">
+        <div
+          className={` absolute sm:static top-20 bg-pink-200 sm:bg-transparent sm:w-auto w-full sm:pl-0 text-center sm:py-0 z-10 sm:shadow-none shadow-lg`}
+        >
+          <ul
+            className={`${
+              !toggle ? "hidden" : "inline"
+            } sm:flex sm:gap-6 w-full`}
+          >
             {Links.map((link, index) => (
-              <li key={index}>
+              <li className="py-2" key={index}>
                 <Link
                   className="text-lg hover:text-teal-500 focus:text-pink-500 duration-200"
                   to={link.link}
@@ -38,18 +51,19 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div>
+        <div className="sm:hidden pr-4 cursor-pointer inline">
           <img
-            className="sm:hidden cursor-pointer"
-            src={bun}
-            alt="hamburger menu"
-          />
-          <img
-            className="sm:hidden cursor-pointer"
-            src={cross}
-            alt="hamburger menu"
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain "
+            onClick={() => setToggle((prev) => !prev)}
           />
         </div>
+
+        {/* <div className="sm:hidden pr-4">
+          <VscClose className="sm:none cursor-pointer text-3xl" />
+          <VscMenu className="sm:none cursor-pointer text-3xl" />
+        </div> */}
       </div>
     </section>
   );
