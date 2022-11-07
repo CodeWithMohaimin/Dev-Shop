8bbc4daf9fe5a1c110a39bda9d36dda43839eec0
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-07 14:15:22 +0600

    init

--- src/components/Markets.jsx
+++ src/components/Markets.jsx
@@ -8,17 +8,17 @@ import images from "../image/image-section.png";
 const Markets = () => {
   const navigate = useNavigate();
   return (
-    <section className="bg-market">
-      <div className="flex justify-center items-center min-h-30 bg-gradient-to-tl from-pink-100 to-gray-200">
-        <h1 className="text-center capitalize text-3xl sm:text-5xl font-semibold sm:font-bold text-gray-600">
+    <section className="bg-market mt-4">
+      <div className="flex justify-center items-center px-2 min-h-30 bg-transparent">
+        <h1 className="text-center capitalize text-4xl sm:text-6xl font-semibold sm:font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-blue-500 border-b-2 border-purple-100 pb-2">
           Welcome To
-          <span className="text-transparent bg-clip-text bg-gradient-to-b from-pink-400 to-orange-300 pl-2">
+          <span className="text-transparent bg-clip-text bg-gradient-to-l from-pink-400 to-orange-300 pl-2">
             Dev. Market
           </span>
         </h1>
       </div>
 
-      <div className="flex justify-center min-h-60 items-center gap-8 flex-wrap my-10 px-2">
+      <div className="flex justify-center min-h-60 items-center gap-8 flex-wrap my-10 px-4">
         {/* card  */}
         <div
           className="shadow-2xl  text-center max-w-md w-full border-pink-200 border-t-4 rounded-2xl cursor-pointer"
@@ -35,7 +35,7 @@ const Markets = () => {
           </div>
           <div className="bg-gradient-to-br from-pink-200 to-blue-200 p-4 mt-6 rounded-b-2xl">
             <h1 className="text-2xl sm:text-3xl capitalize font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-teal-300">
-              navbar
+              navbars
             </h1>
             <p className="text-lg text-gray-500 p-6">
               Here The main Market place for developer's. It's called{" "}
@@ -58,14 +58,14 @@ const Markets = () => {
           <div className="flex justify-center p-4">
             <img className="max-w-xs rounded-lg" src={cardImage} alt="navar" />
           </div>
-          <div className="bg-gradient-to-br from-pink-200 to-blue-200 p-4 mt-6 rounded-b-2xl">
-            <h1 className="text-2xl sm:text-3xl capitalize font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-teal-300">
+          <div className="bg-gradient-to-br from-purple-400 to-red-200 p-4 mt-6 rounded-b-2xl">
+            <h1 className="text-2xl sm:text-3xl capitalize font-bold text-transparent bg-clip-text bg-gradient-to-t from-red-300 to-white">
               Cards
             </h1>
             <p className="text-lg text-gray-500 p-6">
-              Here The main Market place for developer's. It's called{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-orange-200 px-2">
-                Dev. Nav
+              Here The main Market place for developer's. It's called
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-white px-2">
+                Dev. card
               </span>
               A navigation bar is a navigation header that is placed at the top
               of the page
@@ -83,14 +83,14 @@ const Markets = () => {
           <div className="flex justify-center p-4">
             <img className="max-w-xs rounded-lg" src={images} alt="navar" />
           </div>
-          <div className="bg-gradient-to-br from-pink-200 to-blue-200 p-4 mt-6 rounded-b-2xl">
-            <h1 className="text-2xl sm:text-3xl capitalize font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-teal-300">
+          <div className="bg-gradient-to-br from-blue-400 to-teal-400 p-4 mt-6 rounded-b-2xl">
+            <h1 className="text-2xl sm:text-3xl capitalize font-bold text-transparent bg-clip-text bg-gradient-to-tr from-yellow-500 to-purple-600">
               Images
             </h1>
             <p className="text-lg text-gray-500 p-6">
-              Here The main Market place for developer's. It's called{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-orange-200 px-2">
-                Dev. Nav
+              Here The main Market place for developer's. It's called
+              <span className="text-transparent bg-clip-text bg-gradient-to-t from-yellow-500 to-purple-600 px-2">
+                Dev. image
               </span>
               A navigation bar is a navigation header that is placed at the top
               of the page
